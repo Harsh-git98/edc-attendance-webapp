f3e40aed9f1111135066c8a798f85f09295a4d7e
Harsh Ranjan <shriharshranjangupta@gmail.com>
2024-12-29 01:27:35 +0530

    Cleaning code

--- app.js
+++ app.js
@@ -1,3 +1,91 @@
+const express = require('express');//EJS APPLICATION
+const bodyParser = require('body-parser');// TO PARSE DATA
+const axios = require('axios');// TO MAKE HTTP REQUESTS
+const passport = require('passport');// TO AUTHENTICATE USERS
+const session = require('express-session');// TO MAINTAIN SESSIONS
+
+// Create an Express app
+const app = express();//MAIN APP
+
+app.set('view engine', 'ejs'); //EJS ENGINE
+app.set('views', './views');//VIEWS WHERE THE EJS FILES ARE LOCATED
+
+// Static files
+app.use(express.static('public'));//PUBLIC FOLDER WHERE CSS AND JS FILES ARE LOCATED
+
+// Express Session Middleware
+app.use(session({
+    secret: 'your_secret_key',//
+    resave: false,//
+    saveUninitialized: true 
+}));
+
+// Passport initialization
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Fire controllers
+const todoController = require('./controllers/todoController');
+
+const loginController = require('./controllers/logincontrol');
+
+todoController(app);
+loginController(app);
+
+
+const port = 3301;
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+});
+
+
+
+
+
+
+
+//WEBSITE RELOADER RENDER.COM SOLUTION
+
+
+// const url = `https://edc-attendance-app.onrender.com/todo`;
+
+// const interval = 30000;
+
+// function reloadWebsite() {
+//   axios.get(url)
+//     .then(response => {
+//       console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+//     })
+//     .catch(error => {
+//       console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+//     });
+// }
+
+// setInterval(reloadWebsite, interval);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const express = require('express');
 // const bodyParser = require('body-parser');
 // const axios = require('axios');
@@ -42,62 +130,3 @@
 // app.listen(port, () => {
 //     console.log(`Listening to port ${port}`);
 // });
-
-
-
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
-const passport = require('passport');
-const session = require('express-session');
-
-// Create an Express app
-const app = express();
-
-// Setting up the template engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
-// Static files
-app.use(express.static('public'));
-
-// Express Session Middleware
-app.use(session({
-    secret: 'your_secret_key',
-    resave: false,
-    saveUninitialized: true
-}));
-
-// Passport initialization
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Fire controllers
-const todoController = require('./controllers/todoController');
-const loginController = require('./controllers/logincontrol');
-
-todoController(app);
-loginController(app);
-
-// const url = `https://edc-attendance-app.onrender.com/todo`;
-
-// const interval = 30000;
-
-// function reloadWebsite() {
-//   axios.get(url)
-//     .then(response => {
-//       console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
-//     })
-//     .catch(error => {
-//       console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
-//     });
-// }
-
-// setInterval(reloadWebsite, interval);
-
-const port = 3301;
-// Listen to port
-app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
-});
--- controllers/logincontrol.js
+++ controllers/logincontrol.js
@@ -1,3 +1,73 @@
+require('dotenv').config();//ENVIRONMENT VARIABLE
+
+
+const passport = require('passport');//PASSPORT
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;//
+
+module.exports = function(app) {
+
+    // Passport Config
+    passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL
+    }, (accessToken, refreshToken, profile, done) => {
+        return done(null, profile);
+    }));
+
+    passport.serializeUser((user, done) => {
+        done(null, user);
+    });
+
+    passport.deserializeUser((user, done) => {
+        done(null, user);
+    });
+
+    // Routes  
+    // HOME PAGE WILLRENDER LOGIN PAGE
+    app.get('/', (req, res) => {
+        res.render('login');
+    });
+
+    app.get('/auth/google', //GOOGLE AUTHENTICATION
+        passport.authenticate('google', { scope: ['profile'] })
+    );
+
+    // IF AUTHENTICATION IS SUCCESSFUL REDIRECT TO TODO PAGE
+    app.get('/auth/google/callback',
+        passport.authenticate('google', { failureRedirect: '/' }),
+        (req, res) => {
+            res.redirect('/todo');
+        }
+    );
+
+    // IF AUTHENTICATION IS SUCCESSFUL REDIRECT TO TODO PAGE
+    app.get('/todo', (req, res) => {
+        if (req.isAuthenticated()) {
+            res.render('todo', { user: req.user });
+        } else {
+            res.redirect('/');
+        }
+    });
+    // LOGOUT
+    app.get('/logout', (req, res) => {
+        req.logout(err => {
+            if (err) { return next(err); }
+            res.redirect('/');
+        });
+    });
+};
+
+
+
+
+
+
+
+
+
+
 // const express = require('express');
 // const passport = require('passport');
 // const session = require('express-session');
@@ -64,63 +134,3 @@
 
 
 // };
-require('dotenv').config();
-
-
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-module.exports = function(app) {
-
-    // Passport Config
-    passport.use(new GoogleStrategy({
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL
-
-        //  clientID: 'ygygggvh',
-        // clientSecret: 'gvfgvghvgvghvhv',
-        // callbackURL: 'fffgvhvjvhvj'
-    }, (accessToken, refreshToken, profile, done) => {
-        return done(null, profile);
-    }));
-
-    passport.serializeUser((user, done) => {
-        done(null, user);
-    });
-
-    passport.deserializeUser((user, done) => {
-        done(null, user);
-    });
-
-    // Routes
-    app.get('/', (req, res) => {
-        res.render('login');
-    });
-
-    app.get('/auth/google',
-        passport.authenticate('google', { scope: ['profile'] })
-    );
-
-    app.get('/auth/google/callback',
-        passport.authenticate('google', { failureRedirect: '/' }),
-        (req, res) => {
-            res.redirect('/todo');
-        }
-    );
-
-    app.get('/todo', (req, res) => {
-        if (req.isAuthenticated()) {
-            res.render('todo', { user: req.user });
-        } else {
-            res.redirect('/');
-        }
-    });
-
-    app.get('/logout', (req, res) => {
-        req.logout(err => {
-            if (err) { return next(err); }
-            res.redirect('/');
-        });
-    });
-};
